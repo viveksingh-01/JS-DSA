501fcf460bf371b75152214447b8326d22425ab6
viveksingh-01 <viveksingh9301@gmail.com>
2021-11-02 00:47:56 +0530

    Reverse a string - alternate solutions

--- reverse-string/index.js
+++ reverse-string/index.js
@@ -1,5 +1,20 @@
+// SOLUTION - 1
+// function reverseString(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// SOLUTION - 2
+// function reverseString(str) {
+//   let reversedString = '';
+//   for (const char of str) {
+//     reversedString = char + reversedString;
+//   }
+//   return reversedString;
+// }
+
+// SOLUTION - 3
 function reverseString(str) {
-  return str.split('').reverse().join('');
+  return str.split('').reduce((reversedString, char) => char + reversedString, '');
 }
 
 module.exports = reverseString;
